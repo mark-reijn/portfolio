6e9109f4c22696a9336457f68f21c1dca1ce1568
Mark Reijn <mark.reijn.reijn@hva.nl>
2023-03-04 15:53:12 +0100

    Added db connection

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
 import csv
+import json
+import requests
 from flask import Flask, render_template, url_for, request, redirect
 app = Flask(__name__)
+
+def convertToJsonArray(data):
+    array = [];
+    for item in data['items']:
+            array.append({
+                "id":item['track']['id'],
+                "href":item['track']['external_urls']['spotify'],
+                "name":item['track']['name'],
+                "artist":item['track']['artists'][0]['name'],
+                "duration":item['track']['duration_ms'],
+                "image":item['track']['album']['images'][0]['url']
+            })
+    return json.dumps(array)
 
 @app.route('/')
 def homepage():
     return render_template('index.html')
-
 
 @app.route('/<string:page_name>')
 def html_page(page_name):
@@ -37,3 +51,17 @@ def submit_form():
         return redirect('/thankyou.html')
     else:
         return 'Something went wrong!'
+
+@app.route('songs/<string:token>')
+def get_songs(token):
+       
+    response = requests.get(
+        "https://api.spotify.com/v1/playlists/4RpIr7DUkNf99IRy9Wk74H/tracks?market=NL&limit=10&offset=0", 
+        headers={'Authorization':'Bearer '+ token})
+
+    if response.status_code == 200:
+        data = convertToJsonArray(response.json())
+        methods.insertData(data)
+        
+    else: 
+        print(response)
