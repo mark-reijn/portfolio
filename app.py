418aacce2a98f86d466b73ccd06c0b0b5f5c1645
Mark Reijn <mark.reijn.reijn@hva.nl>
2023-03-04 18:38:22 +0100

    Code cleanup

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 import csv
 import json
+from azure.keyvault.secrets import SecretClient
+from azure.identity import DefaultAzureCredential
 from pymongo import MongoClient
 from flask import Flask, render_template, url_for, request, redirect
 app = Flask(__name__)
+
+keyVaultName = "PortfolioVaultMark2023"
+KVUri = f"https://{keyVaultName}.vault.azure.net"
 
 def convertToJsonArray(data):
     array = [];
@@ -18,46 +23,29 @@ def convertToJsonArray(data):
     return json.dumps(array)
 
 def connectToDatabase():
-    user = "markreijn"
-    password = "Rn0TwmhnShIeDrWf"
-    client = MongoClient(f"mongodb+srv://{user}:{password}@portfolio.7sw702i.mongodb.net/?retryWrites=true&w=majority")
-    db = client.spotify
+    credential = DefaultAzureCredential()
+    client = SecretClient(vault_url=KVUri, credential=credential)
+    dbString = client.get_secret("DbConnection")
+
+    mongoClient = MongoClient(f"mongodb+srv://{dbString}")
+    db = mongoClient.spotify
     return db.song
 
-@app.route('/')
-def homepage():
-    return render_template('index.html')
-
-# @app.route('/<string:page_name>')
-# def html_page(page_name):
-#     return render_template(page_name)
-
-
-# def write_to_file(data):
-#     with open('database.txt', mode='a') as database:
-#         email = data['email']
-#         subject = data['subject']
-#         message = data['message']
-#         file = database.write(f'\n{email},{subject},{message}')
-
-
-def write_to_csv(data):
-    with open('database.csv', 'a', newline='') as csvfile:
+def write_to_file(data):
+    with open('database.txt', mode='a') as database:
         email = data['email']
         subject = data['subject']
         message = data['message']
-        writer = csv.writer(csvfile)
-        writer.writerow([email, subject, message])
+        file = database.write(f'\n{email},{subject},{message}')
 
-
-@app.route('/submit_form', methods=['GET', 'POST'])
-def submit_form():
-    if request.method == 'POST':
-        data = request.form.to_dict()
-        write_to_csv(data)
-        return redirect('/thankyou.html')
-    else:
-        return 'Something went wrong!'
+# @app.route('/submit_form', methods=['GET', 'POST'])
+# def submit_form():
+#     if request.method == 'POST':
+#         data = request.form.to_dict()
+#         write_to_csv(data)
+#         return redirect('/thankyou.html')
+#     else:
+#         return 'Something went wrong!'
 
 def songs_from_db():
     collection = connectToDatabase()
@@ -67,14 +55,32 @@ def songs_from_db():
     for song in songs:
         array.append(song)
 
-    print(songs)
-    return(songs)
+    return(array)
+
+@app.route('/')
+def homepage():
+    return render_template('index.html')
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/index")
+def index():
+    return render_template("index.html")
 
 @app.route('/songs')
-def get_songs():
-    collection = connectToDatabase()
-    test = collection.find({}, {'_id': False})
+def songs():
     return render_template(
-        "../templates/songs.html",
-        songs="hallo man"
+        "songs.html",
+        songs=songs_from_db()
     )
+
+@app.route('/works')
+def works():
+    return render_template("works.html")
+
